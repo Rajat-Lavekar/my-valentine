56703667cbe5136f817e1d479065e62d9a9897dd
Rajat Lavekar <rajat.lavekar@gmail.com>
2026-02-11 01:22:38 +0530

    feat: add necessary web asset imports

---
+++ src/data/webAssets.js
@@ -0,0 +1,9 @@
+// Web-sourced assets used for richer visuals in the cinematic intro/cursor.
+export const WEB_ASSETS = {
+  sunflowerField:
+    'https://images.unsplash.com/photo-1660990448406-fd19851c1842?auto=format&fit=crop&fm=jpg&q=80&w=2400',
+  sunflowerPng:
+    'https://purepng.com/download/Sflfyn74ZndPTGwdCX8zUlsOaTYPYX6WN2TznxoDQOMY8vMcx10Jc5hlOn5p8xyY4oENH2m6tnawiw528XSIrsW0IPgtZXORmWDWavBDTWhiBqqgh1bP0k8tysu2BBRR0cqweOxMxCdAPgxYS6icOcpX68DpDK1PmLiLYnDZIjIQnRtcPtHGqFZnvt9H5m9gj9P0RA32/large',
+  crumpledPaperTexture:
+    'https://images.unsplash.com/photo-1715794792806-4c5327d69df8?auto=format&fit=crop&fm=jpg&q=80&w=1800'
+};
